81c21cec79c7a5028fee72cf16bc5a5b641e77e4
Giuseppe De Marco <giuseppe.demarco@unical.it>
2020-04-20 22:53:44 +0200

    v0.2.2

--- auditing/utils.py
+++ auditing/utils.py
@@ -1,8 +1,8 @@
 def get_request_info(request):
     items = dict(request.headers.items())
-    url = items.pop('Origin')  
+    url = request.build_absolute_uri()  
     headers_str = ', '.join(['"{}": "{}"'.format(k, v)
-                             for k,v in items if v])
+                             for k,v in items.items() if v])
     return '"url": "{}", "srcip": "{}", "path": "{}", {}'.format(url,
                                                                  request.META['REMOTE_ADDR'],
                                                                  request.get_full_path(),
