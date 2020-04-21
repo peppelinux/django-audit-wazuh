e8d836c0e5e4e4bb0c88eae7116cddeaaf39905a
Giuseppe De Marco <giuseppe.demarco@unical.it>
2020-04-21 15:14:44 +0200

    real ip even from behind a reverse proxy

--- auditing/utils.py
+++ auditing/utils.py
@@ -1,9 +1,15 @@
 def get_request_info(request):
     items = dict(request.headers.items())
+
+    # get the real ip, even if request come from a reverse proxy
+    srcip = request.META['REMOTE_ADDR']
+    if items.get("X-Forwarded-For") or items.get("X-Real-Ip"):
+        srcip = items.get("X-Forwarded-For") or items.get("X-Real-Ip")
+    
     url = request.build_absolute_uri()  
     headers_str = ', '.join(['"{}": "{}"'.format(k, v)
                              for k,v in items.items() if v])
-    return '"url": "{}", "data.srcip": "{}", "path": "{}", {}'.format(url,
-                                                                      request.META['REMOTE_ADDR'],
-                                                                      request.get_full_path(),
-                                                                      headers_str)
+    return '"url": "{}", "srcip": "{}", "path": "{}", {}'.format(url,
+                                                                 srcip,
+                                                                 request.get_full_path(),
+                                                                 headers_str)
