30ea8d3a4e4f2faa15cb359ee009e166c753a6f1
Andre Engelbrecht <andre@tehnode.co.uk>
2020-09-26 19:42:18 +0100

    Allow for a different "username" field
    We have a project that uses "email" for the username for example.

--- auditing/__init__.py
+++ auditing/__init__.py
@@ -4,22 +4,27 @@ from django.contrib.auth.signals import (user_logged_in,
                                          user_login_failed,
                                          user_logged_out)
 from django.dispatch import receiver
+from django.conf import settings
+
 from . utils import get_request_info
 
+
 logger = logging.getLogger(__name__)
+
+USER_FIELD = getattr(settings, 'AUDIT_USERNAME_FIELD', 'username')
 
 
 @receiver(user_logged_in)
 def login_logger(sender, request, **kwargs):
     _msg = '"Django Login successful", "username": "{}", {}'
-    logger.info(_msg.format(request.POST['username'],
+    logger.info(_msg.format(request.POST[USER_FIELD],
                             get_request_info(request)))
 
 
 @receiver(user_login_failed)
 def login_failed_logger(sender, request, **kwargs):
     _msg = '"Django Login failed", "username": "{}", {}'
-    logger.warn(_msg.format(request.POST['username'],
+    logger.warn(_msg.format(request.POST[USER_FIELD],
                             get_request_info(request)))
 
 
