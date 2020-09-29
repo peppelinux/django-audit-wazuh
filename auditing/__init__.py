f65f512ee4ff9e3284f69d0acf9c1bd3ebbd1fd1
Andre Engelbrecht <andre@tehnode.co.uk>
2020-09-29 21:12:21 +0100

    Added tests, start refactor work on signals
    * Update get_request_info function to work with dict objects, which
      would allow changing and inserting various fields easier
    * Added a message formatter that will take our siem_data (created by
      updated function above) and format it as a valid json string, while
      stripping the open and closing brackets.
    * Updated the signals handlers so that they use our new methods (defined
      above)
    * Added tests for all the above

--- auditing/__init__.py
+++ auditing/__init__.py
@@ -6,30 +6,32 @@ from django.contrib.auth.signals import (user_logged_in,
 from django.dispatch import receiver
 from django.conf import settings
 
-from . utils import get_request_info
+from .utils import get_request_info, format_log_message
 
 
 logger = logging.getLogger(__name__)
 
-USER_FIELD = getattr(settings, 'AUDIT_USERNAME_FIELD', 'username')
-
 
 @receiver(user_logged_in)
-def login_logger(sender, request, **kwargs):
-    _msg = '"Django Login successful", "username": "{}", {}'
-    logger.info(_msg.format(request.POST[USER_FIELD],
-                            get_request_info(request)))
+def login_logger(sender, **kwargs):
+    siem_data = get_request_info(kwargs['request'])
+    siem_data['username'] = kwargs['user'].get_username()
+    logger.info('"Django Login successful", {}'.format(
+        format_log_message(siem_data)))
 
 
 @receiver(user_login_failed)
-def login_failed_logger(sender, request, **kwargs):
-    _msg = '"Django Login failed", "username": "{}", {}'
-    logger.warn(_msg.format(request.POST[USER_FIELD],
-                            get_request_info(request)))
+def login_failed_logger(sender, **kwargs):
+    USER_FIELD = getattr(settings, 'AUDIT_USERNAME_FIELD', 'username')
+    siem_data = get_request_info(kwargs['request'])
+    siem_data['username'] = kwargs['credentials'][USER_FIELD]
+    logger.warn('"Django Login failed", {}'.format(
+        format_log_message(siem_data)))
 
 
 @receiver(user_logged_out)
-def logout_logger(sender, request, **kwargs):
-    _msg = '"Django Logout successful", "username": "{}", {}'
-    logger.info(_msg.format(request.user,
-                            get_request_info(request)))
+def logout_logger(sender, **kwargs):
+    siem_data = get_request_info(kwargs['request'])
+    siem_data['username'] = kwargs['user'].get_username()
+    logger.info('"Django Logout successful", {}'.format(
+        format_log_message(siem_data)))
--- auditing/utils.py
+++ auditing/utils.py
@@ -1,15 +1,30 @@
-def get_request_info(request):
-    items = dict(request.headers.items())
+import json
 
-    # get the real ip, even if request come from a reverse proxy
+
+def format_log_message(msg_data):
+    """
+    :param msg_data: The data message data dict to write as JSON
+    :return: A valid json string without outer brackets to be passed to the
+    python logger
+    """
+    return json.dumps(msg_data)[1:-1]
+
+
+def get_request_info(request):
+    """
+    :param request: The django request object
+    :return: A dictionary that contains the required fields for our siem msg
+    """
+    r_head = dict(request.headers.items())
+
     srcip = request.META['REMOTE_ADDR']
-    if items.get("X-Forwarded-For") or items.get("X-Real-Ip"):
-        srcip = items.get("X-Forwarded-For") or items.get("X-Real-Ip")
-    
-    url = request.build_absolute_uri()  
-    headers_str = ', '.join(['"{}": "{}"'.format(k, v)
-                             for k,v in items.items() if v])
-    return '"url": "{}", "srcip": "{}", "path": "{}", {}'.format(url,
-                                                                 srcip,
-                                                                 request.get_full_path(),
-                                                                 headers_str)
+    srcip = r_head.get("X-Forwarded-For") or r_head.get("X-Real-Ip", srcip)
+
+    url = request.build_absolute_uri()
+    siem_data = {
+        "url": url,
+        "srcip": str(srcip),
+        "path": request.get_full_path(),
+    }
+    siem_data.update(r_head)
+    return siem_data
