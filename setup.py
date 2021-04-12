9be84578aa7d60d4d074a52abdba42525a021a55
peppelinux <giuseppe.demarco@unical.it>
2021-04-12 23:27:27 +0200

    setup.py - django classifiers

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
